c0cbe9c9334e8780d1512624313278839f46c009
lb madesia <lmadesia@gmail.com>
2021-03-26 18:45:57 +0530

    first react practical web app

---
+++ README.md
@@ -0,0 +1,14 @@
+step 1.:- create new react project 
+
+npx create-react-app appname
+
+step 2. :-  bootstrap install 
+
+npm install react-bootstrap bootstrap
+
+step 3. :- copy (public) and (src) in this file 
+
+step 4. :- paste in own (new) react project  
+
+public   
+src
--- src/From.js
+++ src/From.js
@@ -1,26 +1,31 @@
+import React, { Component } from "react";
 import Name from "./component/Name";
-
-
-const From = () => {
-   let state = {
+class From extends Component {
+    state = {
         items : [],
         text : "lb madesia"
     }
-    const addname = () => {
-        console.log(state.text);
-       state.text = "vivek has been changed";
-       console.log(state.text);
+   addname = () => {
+       let indata = document.getElementById("in_name").value;
+       let item = [...this.state.items,indata];
+       this.setState({items : item});
     }
-   
+    deleteName = (id) => {
+      let item = this.state.items.filter((element,i)=>{
+          return i !== id
+      })
+      this.setState({items : item});
+    }
+   render(){
     return (
       <div className="w-100 bg-dark">
         <div className="p-4 bg-light shadow-lg d-flex justify-content-center mb-4">
             <div className="w-50">
                 <h3 className="text-center">Test App</h3>
                 <div className="input-group">
-                    <input type="text" name="name" placeholder="enter name" className="form-control" />
+                    <input type="text" name="name" id="in_name" placeholder="enter name" className="form-control" />
                     <div className="input-group-append">
-                        <span className="input-group-text" onClick={addname}>ADD</span>
+                        <span className="input-group-text" onClick={this.addname}>ADD</span>
                     </div>
                 </div>
             </div>
@@ -30,12 +35,18 @@ const From = () => {
             <div className="w-50">
                     <ul className="list-group">
                         <li className="list-group-item d-flex justify-content-between align-items-center"><p>Name List</p><i className="fa fa-close text-danger"></i></li>
-                       <Name name={state.text} />
+                       {
+                           this.state.items.map((value , i) =>{
+                               return <Name key={i} name={value} id={i} delete={ this.deleteName } />
+                           })
+                       }
+
                     </ul>
             </div>
         </div>
 
       </div>
     );
+}
 }
 export default From;
